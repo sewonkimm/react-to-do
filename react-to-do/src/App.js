32a4ac5cdac58585ba617ff01969a8d7b4bc2ac6
SeWonKim <30452963+Siromom@users.noreply.github.com>
2020-04-27 18:41:07 +0900

    [Add] TodoListItem component 추가

--- react-to-do/src/App.js
+++ react-to-do/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <TodoTemplate>
       <TodoInsert />
+      <TodoList />
     </TodoTemplate>
   );
 }
---
+++ react-to-do/src/components/TodoListItem.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './TodoListItem.scss';
+import { MdCheckBoxOutlineBlank, MdRemoveCircleOutline } from 'react-icons/md';
+
+function TodoListItem(props) {
+  return (
+    <div className="TodoListItem">
+      <div className="checkbox">
+        <MdCheckBoxOutlineBlank />
+        <div className="text">할 일</div>
+      </div>
+      <div className="remove">
+        <MdRemoveCircleOutline />
+      </div>
+    </div>
+  );
+}
+
+export default TodoListItem;
