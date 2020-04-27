1fd0980a73da8ae998e8cea498e339e8e0041b49
SeWonKim <30452963+Siromom@users.noreply.github.com>
2020-04-27 20:17:24 +0900

    [Add] 기능 추가
    1. Insert 기능
    2. Remove 기능
    3. Toggle 기능

--- react-to-do/src/components/TodoList.js
+++ react-to-do/src/components/TodoList.js
@@ -2,12 +2,17 @@ import React from 'react';
 import './TodoList.scss';
 import TodoListItem from './TodoListItem';
 
-function TodoList(props) {
+function TodoList({ todos, onRemove, onToggle }) {
   return (
     <div className="TodoList">
-      <TodoListItem />
-      <TodoListItem />
-      <TodoListItem />
+      {todos.map((todo) => (
+        <TodoListItem
+          todo={todo}
+          key={todo.id}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
+      ))}
     </div>
   );
 }
