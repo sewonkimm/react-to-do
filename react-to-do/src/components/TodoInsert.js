b3b3a9fa9794b3d144aee1e090828d3d1a650358
SeWonKim <30452963+Siromom@users.noreply.github.com>
2020-04-27 17:18:57 +0900

    [Fix] placeholder 오타 수정

--- react-to-do/src/components/TodoInsert.js
+++ react-to-do/src/components/TodoInsert.js
@@ -5,7 +5,7 @@ import { MdAdd } from 'react-icons/md';
 function TodoInsert(props) {
   return (
     <form className="TodoInsert">
-      <input placehodler="할 일을 입력하세요!" />
+      <input placeholder="할 일을 입력하세요!" />
       <button type="submit">
         <MdAdd />
       </button>
