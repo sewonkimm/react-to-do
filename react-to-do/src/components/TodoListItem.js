5f33f091eab4364b2f16776a5b5046e25dbfcadb
SeWonKim <30452963+Siromom@users.noreply.github.com>
2020-05-04 14:31:13 +0900

    [Feat] react-virtualized 사용해서 렌더링 최적화

--- react-to-do/src/components/TodoListItem.js
+++ react-to-do/src/components/TodoListItem.js
@@ -7,20 +7,28 @@ import {
 } from 'react-icons/md';
 import cn from 'classnames';
 
-function TodoListItem({ todo, onRemove, onToggle }) {
+function TodoListItem({ todo, onRemove, onToggle, style }) {
   const { id, text, checked } = todo;
 
   return (
-    <div className="TodoListItem">
-      <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
-        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
-        <div className="text">{text}</div>
-      </div>
-      <div className="remove" onClick={() => onRemove(id)}>
-        <MdRemoveCircleOutline />
+    <div className="TodoListItem-virtualized" style={style}>
+      <div className="TodoListItem">
+        <div
+          className={cn('checkbox', { checked })}
+          onClick={() => onToggle(id)}
+        >
+          {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+          <div className="text">{text}</div>
+        </div>
+        <div className="remove" onClick={() => onRemove(id)}>
+          <MdRemoveCircleOutline />
+        </div>
       </div>
     </div>
   );
 }
 
-export default React.memo(TodoListItem);
+export default React.memo(
+  TodoListItem,
+  (prevProps, nextProps) => prevProps.todo === nextProps.todo,
+);
